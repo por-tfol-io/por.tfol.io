0e2e6e20ac136234a170b7e8cab15fde0332022a
eliranmal <eliranmal@gmail.com>
2019-04-25 20:30:38 +0300

    feature(views): branding
    add home page view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<Icon class="icon" action="loop"/>-->
+
+    <!-- todo - start navigation from here. -->
+    <!-- todo - navigation concept: -->
+    <!-- todo - the big icon in the center of the home page leads to the about page, -->
+    <!-- todo - which is styled the same, but the container is almost entirely opaque (.8), -->
+    <!-- todo - so the icon can be seen through -->
+    <!-- todo - the about section is arranged as changing (sliding) cards, when the last one -->
+    <!-- todo - will invite (and lead) to the gallery -->
+
+    <!--<Icon class="icon" action="toss"/>-->
+    <Icon class="icon" name="magic-wand"/>
+    <!--<Icon class="icon" name="rocket"/>-->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Icon from '@/components/Icon.vue';
 
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
-    HelloWorld,
+    Icon,
   },
 };
 </script>
+
+<style scoped lang="scss">
+
+  .home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+
+    .icon {
+      font-size: 20vw;
+      color: var(--color-accent-1);
+    }
+  }
+
+</style>
