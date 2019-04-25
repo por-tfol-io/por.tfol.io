e3f3224313cc0e078f60acee83d045e804a111b0
eliranmal <eliranmal@gmail.com>
2019-04-25 18:12:53 +0300

    feature(components): layout
    add accordion slider component

---
+++ src/components/AccordionSlider.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="accordion">
+    <router-link
+      class="item"
+      v-for="item in items"
+      :key="item.name"
+      :to="item.link"
+      :style="{
+        width: `${100 / items.length}%`,
+      }"
+    >
+      <h2>{{ item.title }}</h2>
+      <p>{{ item.description }}</p>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccordionSlider',
+  props: {
+    items: {
+      type: Array,
+      default: [],
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import "../styles/global";
+
+  .accordion {
+    display: flex;
+    flex-direction: row;
+    align-items: stretch;
+    justify-content: stretch;
+
+    .item {
+      box-sizing: border-box;
+      position: relative;
+      height: 100%;
+      flex-grow: 1;
+      margin: 0 0 0 2px;
+      padding: $layout-pad 1rem;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      text-decoration: none;
+      /*transform: skew(-5deg);*/
+      transform-origin: 100% 0;
+      transition: width 800ms ease-in-out, padding 800ms ease-in-out;
+
+      h2 {
+        margin-bottom: $layout-pad;
+        white-space: nowrap;
+        word-break: keep-all;
+      }
+
+      &:hover {
+        width: 200% !important;
+        padding: $layout-pad;
+      }
+
+      &:after {
+        content: '';
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: transparent linear-gradient(0deg, transparent, #fff);
+        opacity: .2;
+        pointer-events: none;
+      }
+
+      :first-child {
+        margin-top: 0;
+      }
+
+      :last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+
+</style>
