6f6d7fde590b2718304effdc08820a88fa55ea02
eliranmal <eliranmal@gmail.com>
2019-04-25 20:32:35 +0300

    feature(routing): branding
    add routes for new views, enrich route metadata with colors

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import About from './views/About.vue';
+import Gallery from './views/Gallery.vue';
+import Contact from './views/Contact.vue';
 
 Vue.use(Router);
 
@@ -14,10 +17,68 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: About,
+      meta: {
+        fgColor: [105, 170, 197], // base color
+        bgColor: [34, 38, 37], // background color
+      },
+    },
+    {
+      path: '/gallery',
+      name: 'gallery',
+      component: Gallery,
+      // consider route level code-splitting for the gallery
+      // component: () => import(/* webpackChunkName: "wat" */ './views/Wat.vue'),
+      meta: {
+        fgColor: [242, 235, 220], // content color
+        bgColor: [105, 170, 197], // base color
+      },
+      children: [
+        {
+          path: 'dot-css',
+          name: 'dot.css',
+          meta: {
+            url: 'https://eliranmal.github.io/dot.css/',
+          },
+        },
+        {
+          path: 'cinematic',
+          name: 'cinematic',
+          meta: {
+            url: 'https://cinematic-8d9a1mf4w.now.sh/',
+          },
+        },
+        {
+          path: 'foo-bar',
+          name: 'foo bar',
+          meta: {
+            url: '',
+          },
+        },
+        {
+          path: 'pumpernickel',
+          name: 'pumpernickel',
+          meta: {
+            url: '',
+          },
+        },
+        {
+          path: 'wat',
+          name: 'wat',
+          meta: {
+            url: '',
+          },
+        },
+      ],
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact,
+      meta: {
+        fgColor: [140, 20, 30], // accent 2 color
+        bgColor: [242, 235, 220], // content color
+      },
     },
   ],
 });
