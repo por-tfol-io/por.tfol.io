136fd7f1bc54220f99c93a88c6980c08f259d292
eliranmal <eliranmal@gmail.com>
2019-04-28 19:12:11 +0300

    feature(views): branding
    refine header layout and typography

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="header">
+    <h1 class="title">
+      <span class="small">{{ i18n.title[0] }}</span>
+      <br/>{{ i18n.title[1] }}
+    </h1>
+    <h1 class="subtitle">
+      <router-link to="/">
+        <span class="small">{{ i18n.subtitle[0] }}</span>
+        <br/>{{ i18n.subtitle[1] }}
+      </router-link>
+    </h1>
     <nav>
       <router-link to="/about">about</router-link>
       <router-link to="/gallery">gallery</router-link>
       <router-link to="/contact">contact</router-link>
     </nav>
-    <h1 class="title">
-      <router-link to="/">
-        <span class="small">{{ i18n.title[0] }}</span>
-        <br/>{{ i18n.title[1] }}
-      </router-link>
-    </h1>
-    <h1 class="subtitle">
-      <span class="small">{{ i18n.subtitle[0] }}</span>
-      <br/>{{ i18n.subtitle[1] }}
-    </h1>
   </div>
 </template>
 
@@ -26,7 +26,6 @@ export default {
     return {
       i18n: {
         title: ['hi, my name is', 'eliran'],
-        // subtitle: ['this is my programming', 'portfolio'],
         subtitle: ['this is my software development', 'portfolio'],
       },
     };
@@ -39,68 +38,64 @@ export default {
   .header {
     display: flex;
     flex-direction: column;
-    align-items: end;
+    align-items: flex-end;
     justify-content: stretch;
     text-align: center;
     text-transform: uppercase;
     font-size: 1.04rem;
 
-    > * {
+    > h1,
+    > nav {
       margin: 0;
     }
 
-    nav {
-      align-self: flex-start;
+    a:not(.router-link-active) {
+      opacity: var(--opacity-reduced);
+    }
+
+    .title,
+    .subtitle {
+      margin-top: -.35em;
+      font-size: 2.6em;
+
+      .small {
+        display: inline-block;
+        white-space: nowrap;
+        word-break: keep-all;
+        margin-bottom: 1.5rem;
+        line-height: 0;
+        font-weight: bold;
+      }
+    }
+
+    .title {
+      flex-grow: .2;
+      /*font-size: 4.7em;*/
+
+      .small {
+        font-size: .45em;
+      }
+    }
+
+    .subtitle {
       flex-grow: 1;
-      font-size: .7rem;
+
+      .small {
+        font-size: .3em;
+      }
+    }
+
+    nav {
+      flex-grow: 0;
+      font-size: .8rem;
 
       a {
         border-bottom: 2px solid;
-        text-decoration: none;
-        font-weight: bold;
 
         &:not(:last-child) {
           margin-right: .5em;
         }
       }
-    }
-
-
-    .small {
-      display: inline-block;
-      white-space: nowrap;
-      word-break: keep-all;
-    }
-
-    .title {
-      /*font-size: 4.7em;*/
-      font-size: 2.6em;
-      align-self: flex-end;
-      flex-grow: 1;
-
-      .small {
-        font-size: .465em;
-      }
-    }
-
-    .subtitle {
-      /*margin-top: -.35em;*/
-      font-size: 2.6em;
-
-      .small {
-        margin-bottom: .8em;
-        font-size: .32em;
-      }
-    }
-
-    .title {
-      a {
-        text-decoration: none;
-      }
-    }
-
-    a:not(.router-link-exact-active) {
-      opacity: .5;
     }
   }
 
