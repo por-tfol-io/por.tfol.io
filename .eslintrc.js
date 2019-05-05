ec7608426ba0d338ea447431621fc99b6a23d8a7
eliranmal <eliranmal@gmail.com>
2019-05-05 23:55:29 +0300

    feature(dx): branding
    switch off linting for arrow functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'object-curly-newline': 'off',
+    'arrow-body-style': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
