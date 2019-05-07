a897a60296918819984cb20805d6ba87bb627404
eliranmal <eliranmal@gmail.com>
2019-05-07 23:30:48 +0300

    feature(views): branding
    gallery-view:
    
    - add controls
    - proper layout for controls
    - implement empty view

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -5,24 +5,42 @@
       :url="url"
       :isFullscreen.sync="isExpanded"
     />
-    <div v-else># broken link #</div>
+    <div v-else
+         class="empty-view">
+      <Icon name="unlink"/>
+      <span v-for="line in i18n.brokenLink" :key="line">{{ line }}</span>
+    </div>
     <div class="fill"></div>
     <div class="controls">
-        <span class="control expand"
-              tabindex="0"
-              :class="{
-                disabled: !url,
-              }"
-              v-on="{ click: expand }">
-          <Icon name="frame-expand"/>
-        </span>
       <router-link class="control"
                    :to="prevPath">
-        <Icon name="chevron-left"/>
+        <Icon name="arrow-left"/>
+        {{ i18n.label.prev }}
       </router-link>
       <router-link class="control"
                    :to="nextPath">
-        <Icon name="chevron-right"/>
+        <Icon name="arrow-right"/>
+        {{ i18n.label.next }}
+      </router-link>
+      <span class="control expand"
+            tabindex="0"
+            :class="{
+              disabled: !url,
+            }"
+            v-on="{ click: expand }">
+        <Icon name="frame-expand"/>
+        {{ i18n.label.expand }}
+      </span>
+      <span class="control info"
+            tabindex="0"
+            v-on="{ click: showInfo }">
+        <Icon name="question-circle"/>
+        {{ i18n.label.info }}
+      </span>
+      <router-link class="control exit"
+                   to="/gallery">
+        <Icon name="exit"/>
+        {{ i18n.label.exit }}
       </router-link>
     </div>
   </div>
@@ -47,12 +65,28 @@ export default {
   },
   data() {
     return {
+      i18n: {
+        brokenLink: [
+          'this item seems to be broken...',
+          'try another one, or let me know.',
+        ],
+        label: {
+          prev: 'prev',
+          next: 'next',
+          expand: 'expand',
+          info: 'info',
+          exit: 'exit',
+        },
+      },
       isExpanded: false,
     };
   },
   methods: {
     expand() {
       this.isExpanded = true;
+    },
+    showInfo() {
+      // todo - implement with an overlay semi-transparent dialog, add links to github etc. inside
     },
   },
   computed: {
@@ -77,7 +111,8 @@ export default {
 <style scoped lang="scss">
   @import '../styles/globals';
 
-  $control-size: .8rem;
+  $text-size: 1rem;
+  $icon-size: $text-size * 1.5;
 
   .gallery-view {
     display: flex;
@@ -86,6 +121,12 @@ export default {
     justify-content: stretch;
     height: 100%;
 
+    .icon {
+      display: inline-block;
+      margin: 0 0 .5em;
+      font-size: 6rem;
+    }
+
     .web-view {
       width: calc(100vw - #{$header-size} - #{$layout-pad * 2});
       height: calc((100vw - #{$header-size} - #{$layout-pad * 2}) * .55);
@@ -93,40 +134,63 @@ export default {
       border: 3px solid;
     }
 
+    .empty-view {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 100%;
+
+      .icon {
+        font-size: $icon-size * 3;
+      }
+    }
+
     .fill {
       flex-grow: 1;
     }
 
     .controls {
+      flex-grow: 0;
+      align-self: stretch;
       display: flex;
       flex-direction: row;
       align-items: center;
-      flex-grow: 0;
-      margin-top: $control-size * 1.5;
+      justify-content: stretch;
+      margin-top: $icon-size;
 
       .control {
-        margin: 0;
-        font-size: $control-size;
+        flex-grow: 1;
+        // stretch all controls with equal widths
+        width: 100%;
+        margin: 0 $text-size 0 0;
+        outline: 0 none;
+        text-align: center;
+        font-size: $text-size;
         cursor: pointer;
+        opacity: var(--opacity-reduced);
+        text-shadow: 1px 1px 3px #ccc;
 
-        &:not(:last-child) {
-          margin-right: $control-size * 1.5;
+        &:last-child {
+          margin-right: 0;
+        }
+
+        &:hover,
+        &:active {
+          opacity: 1;
         }
 
         &.disabled {
-          opacity: var(--opacity-reduced);
+          opacity: calc(var(--opacity-reduced) / 1.5);
           pointer-events: none;
         }
 
-        &.expand {
-          font-size: $control-size * 1.25;
-        }
-
         .icon {
-          font-weight: bold;
-          text-shadow: 1px 1px 3px #999;
+          font-size: $icon-size;
         }
       }
     }
   }
+
 </style>
